316bbebe08cbb376de9d7287985d046e1023dcd1
Esteban Suarez Prado <estebansuarezprado420@gmail.com>
2024-02-22 17:25:35 -0400

    started components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'product',
+    loadComponent: () => import('./product/product.component'),
+    children: [
+        {
+            path: 'list',
+            title: 'List of products',
+            loadComponent: () => import('./product/pages/product-list/product-list.component').then(c => c.ProductListComponent),
+            data: { animation: 'openClosePage' }
+        },
+        {
+            path: 'new',
+            title: 'New Product',
+            loadComponent: () => import('./product/pages/new-product/new-product.component'),
+            data: { animation: 'statusPage' }
+
+        },
+        {
+            path: 'edit/:id',
+            title: 'Edit products',
+            loadComponent: () => import('./product/pages/edit-product/edit-product.component'),
+            data: { animation: 'togglePage' }
+        },
+        {
+            path: '',
+            redirectTo: 'list',
+            pathMatch: 'full',
+        }]
+
+},
+{
+    path: '',
+    redirectTo: 'product',
+    pathMatch: 'full'
+}];
