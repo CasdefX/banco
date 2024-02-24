fb19798c645f6fe4a33b87672ed01008dbf4c372
Esteban Suarez Prado <estebansuarezprado420@gmail.com>
2024-02-24 07:04:34 -0400

    authorId login interceptor

---
+++ src/app/core/interceptors/login.interceptor.ts
@@ -0,0 +1,6 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const loginInterceptor: HttpInterceptorFn = (req, next) => {
+  req = req.clone({ headers: req.headers.set('authorId', "1") });
+  return next(req);
+};
